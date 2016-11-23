8cf2ed82fa4b045b374a869afd41fddfaa5c6b70
Mai Anh Bao <chuyendungdangki@yahoo.com>
2016-11-23 17:08:46 +0700

    test

--- fb_chatbot/urls.py
+++ fb_chatbot/urls.py
@@ -1,6 +1,7 @@
 # fbchatbot/fb_chatbot/urls.py
 from django.conf.urls import include, url
 from .views import FbBotView
+from . import views
 urlpatterns = [
 		url(r'^test/?$', FbBotView.as_view()),
 		url(r'^$', views.index),
