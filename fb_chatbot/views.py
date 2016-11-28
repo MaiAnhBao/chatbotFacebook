7701c492e12467b5a8c4974ff3c717f70299ca78
Mai Anh Bao <chuyendungdangki@yahoo.com>
2016-11-28 16:35:10 +0700

    fix

--- fb_chatbot/views.py
+++ fb_chatbot/views.py
@@ -39,10 +39,10 @@ def verifyRequestSignature(request, response, buf):
 			print("Couldn't validate the request signature")
 	
 def post_facebook_message(fbid, received_message):
-	user_details_url = "https://graph.facebook.com/v2.6/%s"%fbid
-	user_details_params = {'fields':'first_name,last_name,profile_pic', 'access_token':PAGE_ACCESS_TOKEN}
-	user_details = requests.get(user_details_url,user_details_params).json()
-	post_message_url = 'https://graph.facebook.com/v2.6/me/message?access_token=%s'%PAGE_ACCESS_TOKEN 
+#	user_details_url = "https://graph.facebook.com/v2.6/%s"%fbid
+#	user_details_params = {'fields':'first_name,last_name,profile_pic', 'access_token':PAGE_ACCESS_TOKEN}
+#	user_details = requests.get(user_details_url,user_details_params).json()
+#	post_message_url = 'https://graph.facebook.com/v2.6/me/message?access_token=%s'%PAGE_ACCESS_TOKEN 
 	response_msg = json.dumps({"recipient":{"id":fbid},"message":{"text":received_message}})
 	status = requests.post(post_message_url, headers = {"Content-Type": "application/json"},data=response_msg)
 	print(status.json())
