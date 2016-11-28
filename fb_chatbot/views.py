e83ece40c7032ba9adba0744ef3f541732411491
Mai Anh Bao <chuyendungdangki@yahoo.com>
2016-11-28 17:23:24 +0700

    fix

--- fb_chatbot/views.py
+++ fb_chatbot/views.py
@@ -75,5 +75,6 @@ class FbBotView(generic.View):
 						print("Decode error")
 					print("sender: ", message['sender']['id'])
 					print("msg: ", message['message']['text'])
+					print("recipient ", message['recipient']['id'])
 					post_facebook_message(message['recipient']['id'],message['message']['text'])
 		return HttpResponse()
