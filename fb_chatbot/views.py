6b4e9494f97e91f3f9dfa056706cbb785b835631
Mai Anh Bao <chuyendungdangki@yahoo.com>
2016-11-29 15:06:51 +0700

    demo

--- fb_chatbot/views.py
+++ fb_chatbot/views.py
@@ -44,10 +44,10 @@ def post_facebook_message(fbid, received_message):
 #	user_details_params = {'fields':'first_name,last_name,profile_pic', 'access_token':PAGE_ACCESS_TOKEN}
 #	user_details = requests.get(user_details_url,user_details_params).json()
 	post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN 
-#	response_msg = Message(fbid)
-#	response_msg.makeTextMessage("How do you turn this on?")
-	response_msg = json.dumps({"recipient":{"id":fbid},"message":{"text":received_message}})
-#	print(response_msg)
+	response_msg = Message(fbid)
+	response_msg.makeTextMessage("How do you turn this on?")
+#	response_msg = json.dumps({"recipient":{"id":fbid},"message":{"text":received_message}})
+	print(response_msg)
 	status = requests.post(post_message_url, headers = {"Content-Type": "application/json"},data=response_msg)
 #	print(status.json())
 
