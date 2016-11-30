7c0b218feab64b457d9d2ce3a797253871a7e3ef
Mai Anh Bao <chuyendungdangki@yahoo.com>
2016-11-30 09:45:56 +0700

    fix

--- fbchatbot/settings.py
+++ fbchatbot/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 # Update database configuration with $DATABASE_URL
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASE['default'].update(db_from_env)
+DATABASES['default'].update(db_from_env)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
