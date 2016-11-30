fd0277c82b560d9f90ff2db3c12950fbbe2e3746
Mai Anh Bao <chuyendungdangki@yahoo.com>
2016-11-30 09:28:34 +0700

    add db

--- fb_chatbot/models.py
+++ fb_chatbot/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class UserDetail(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer_text = models.CharField(max_length=200)
+
+class Message(models.Model):
+    message_text = models.CharField(max_length=300)
+    user_id = models.CharField(max_length=50)    
