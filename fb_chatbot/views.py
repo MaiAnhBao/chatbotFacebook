ab99917957b461e1d42f94de3ab1a08cf429d481
Mai Anh Bao <chuyendungdangki@yahoo.com>
2016-12-01 10:22:57 +0700

    update

--- fb_chatbot/views.py
+++ fb_chatbot/views.py
@@ -23,34 +23,24 @@ def error404(request):
 	context = Context({"message":"All: %s" % request,})
 	return HttpResponse(content=template.render(context), content_type='text/html;charset=utf8', status=404)
 
-def verifyRequestSignature(request, response, buf):
-	signature = request.headers['x-hub-signature']
-
-	if not signature:
-		print("Couldn't validate the signature")
-	else:
-		elements = signature.splits('=')
-		method = elements[0]
-		signatureHash = elements[1]
-
-		m = hashlib.sha1()
-		m.update(APP_SECRET)
-		expectedHash = m.hexdigest()
-#		expectedHash = hmac.new(APP_SECRET,signatureHash,sha1).digest().encode("base64").rstrip('\n')
-		if signatureHash != expectedHash:
-			print("Couldn't validate the request signature")
-	
 def post_facebook_message(received_message):
-#	user_details_url = "https://graph.facebook.com/v2.6/%s"%fbid
-#	user_details_params = {'fields':'first_name,last_name,profile_pic', 'access_token':PAGE_ACCESS_TOKEN}
-#	user_details = requests.get(user_details_url,user_details_params).json()
-	post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN 
-	
+	post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN	
 #	response_msg = json.dumps({"recipient":{"id":fbid},"message":{"text":received_message}})
 # 	print("Send message: ",response_msg)
-	status = requests.post(post_message_url, headers = {"Content-Type": "application/json"},data=received_message)
-#	print(status.json())
-
+	try:
+		r = requests.post(post_message_url, headers = {"Content-Type": "application/json"},data=received_message)
+		err = r.raise_for_status()
+	except requests.exceptions.Timeout:
+		print("Timeout")
+	except requests.exceptions.TooManyRedirects:
+		print("Too many redirects")
+	except requests.exceptions.HTTPError as e:
+		print(e)
+	except requests.exceptions.RequestException as e:
+		print(e)
+	if not err and r.status_code == 200:
+		print(r.json())
+		
 class FbBotView(generic.View):
 	def get(self, request, *args, **kwargs):
 		try:
@@ -59,6 +49,7 @@ class FbBotView(generic.View):
 			else:
 				return HttpResponse("Bonjour, erreur jeton invalide")
 		except Exception as e:
+			print(e)
 			return HttpResponse("Cannot get the value token")
 	@method_decorator(csrf_exempt)
 	def dispatch(self, request, *args, **kwargs):
@@ -69,20 +60,52 @@ class FbBotView(generic.View):
 		print("incoming message", incoming_message)		
 		for entry in incoming_message['entry']:
 			for message in entry['messaging']:
-				receivedMsg = message['message']
+				receivedMsg = message['message']['text']
+				sender_user_id = message['sender']['id']
 # 				print("sender: ", message['sender']['id'])
 # 				print("msg: ", message['message']['text'])
 # 				print("recipient ", message['recipient']['id'])
-#				m = MessageDB(message_text = message['message']['text'],user_id=message['sender']['id'])
-#				m.save()
-				response_msg = Message(message['sender']['id'])
 				if receivedMsg:
-					if 'image' in receivedMsg:
-						response_msg.makeAttachmentMessage('http://google.com','image')
+					if 'hello' in receivedMsg or 'hi' in receivedMsg:
+						sendGreetingMessage(sender_user_id)
+					elif 'image' in receivedMsg:						
+# 						response_msg.makeAttachmentMessage('http://google.com','image')
+						sendAttachmentMessage(sender_user_id, 'image')
 					elif 'typing on' in receivedMsg:
-						response_msg.makeTypingOnMessage()
+						sendTypingOnMessage(sender_user_id)
 					else:
-						response_msg.makeTextMessage("How do you turn this on?")
-				print(response_msg.getMessage())
-				post_facebook_message(json.dumps(response_msg.getMessage()))
+						sendTextMessage(sender_user_id, "How do you turn this on?")
 		return HttpResponse()
+
+
+def sendGreetingMessage(userId):
+	greeting_lst = ['Hello', 'Hi', 'Hola']
+	
+	user_details_url = "https://graph.facebook.com/v2.6/%s"%userId
+	user_details_params = {'fields':'first_name,last_name,profile_pic', 'access_token':PAGE_ACCESS_TOKEN}
+	user_details = requests.get(user_details_url,user_details_params).json()
+	name = user_details['first_name']
+	
+	response_msg_text = greeting_lst[random.randint(len(greeting_lst))]  + name
+	response_msg = Message(userId)
+	response_msg.makeTextMessage(response_msg_text)
+	response_msg_text = response_msg.getMessage()
+	post_facebook_message(response_msg_text)
+
+def sendAttachmentMessage(userId, type_msg):
+	response_msg = Message(userId)
+	response_msg.makeAttachmentMessage("http://google.com",type_msg)
+	response_msg_text = response_msg.getMessage()
+	post_facebook_message(response_msg_text)
+	
+def sendTypingOnMessage(userId):
+	response_msg = Message(userId)
+	response_msg.makeTypingOnMessage()
+	response_msg_text = response_msg.getMessage()
+	post_facebook_message(response_msg_text)
+	
+def sendTextMessage(userId, message):
+	response_msg = Message(userId)
+	response_msg.makeTextMessage(message)
+	response_msg_text = response_msg.getMessage()
+	post_facebook_message(response_msg_text)
