d85706a4dc5cf3282ad9f14d6c12f6ebe6a0f2f8
Mai Anh Bao <chuyendungdangki@yahoo.com>
2016-12-01 15:06:13 +0700

    update

--- fb_chatbot/views.py
+++ fb_chatbot/views.py
@@ -65,7 +65,7 @@ class FbBotView(generic.View):
 		for entry in incoming_message['entry']:
 			if 'message' not in entry['messaging']:
 				print("Blah Blah")
-				sender_user_id = entry['message']['sender']['id']
+				sender_user_id = entry['messaging']['sender']['id']
 				print(sender_user_id)				
 				sendTextMessage(sender_user_id, "blah blah")
 			for message in entry['messaging']:
